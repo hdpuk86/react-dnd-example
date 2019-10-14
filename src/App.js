715ac66f774576f8adea8a434c35bb37cae8c36c
Hayley <hayley.d.prior@gmail.com>
2019-10-14 14:58:44 +0100

    add sortable list of cards

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import { DndProvider } from 'react-dnd';
 import logo from './logo.svg';
 import Item from './Item';
 import Target from './Target';
+import Card from './Card';
 import './App.css';
-
+const update = require('immutability-helper');
 
 class App extends Component {
   state = {
@@ -14,6 +15,37 @@ class App extends Component {
       { id: 2, name: 'Item 2' },
       { id: 3, name: 'Item 3' },
       { id: 4, name: 'Item 4' }
+    ],
+    cards: [
+      {
+        id: 1,
+        text: 'Write a cool JS library',
+      },
+      {
+        id: 2,
+        text: 'Make it generic enough',
+      },
+      {
+        id: 3,
+        text: 'Write README',
+      },
+      {
+        id: 4,
+        text: 'Create some examples',
+      },
+      {
+        id: 5,
+        text:
+          'Spam in Twitter and IRC to promote it (note that this element is taller than the others)',
+      },
+      {
+        id: 6,
+        text: '???',
+      },
+      {
+        id: 7,
+        text: 'PROFIT',
+      },
     ]
   }
 
@@ -28,6 +60,20 @@ class App extends Component {
     });
   }
 
+  moveCard = (dragIndex, hoverIndex) => {
+    const { cards } = this.state
+    const dragCard = cards[dragIndex]
+
+    this.setState(
+      update(this.state, {
+        cards: {
+          $splice: [[dragIndex, 1], [hoverIndex, 0, dragCard]],
+        },
+      }),
+    )
+    console.log(this.state.cards)
+  }
+
   render() {
     return(
       <DndProvider backend={HTML5Backend}>
@@ -39,15 +85,26 @@ class App extends Component {
 
           <div className="App-intro">
             <div className="app-container">
-              <div className="item-container">
+              {/* <div className="item-container">
                   {this.state.items.map((item, index) => (
                     <Item key={item.id} item={item} handleDrop={(id) => {
                       this.deleteItem(id)
                     }}/>
                   ))}
-              </div>
+              </div> */}
 
-              <Target />
+              {/* <Target /> */}
+              <div className="card-container">
+                {this.state.cards.map((card, i) => (
+                  <Card
+                    key={card.id}
+                    index={i}
+                    id={card.id}
+                    text={card.text}
+                    moveCard={this.moveCard}
+                  />
+                ))}
+              </div>
             </div>
           </div>
         </div>
